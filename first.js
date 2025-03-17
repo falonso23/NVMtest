848975820f9207b406d930daecd3eea47d4aea6c
Felipe Alonso <alonso.felipe23@gmail.com>
2025-03-17 20:47:04 -0300

    docs

--- first.js
+++ first.js
@@ -2,7 +2,7 @@ async function main() {
 	const req = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
 	if(req.ok) {
 		const data = await req.json();
-		console.log(data);
+		console.log(data.docs[0]);
 	}
 }
 
